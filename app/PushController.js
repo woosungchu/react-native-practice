c6bad691b169e4d725473ead7b29031ae257833a
woosungchu <woosungchu@gmail.com>
2017-03-21 15:10:14 +0900

    FCM getToken()

--- app/PushController.js
+++ app/PushController.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Picker, AppState, Platform } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
+import FCM, {FCMEvent, RemoteNotificationResult, WillPresentNotificationResult, NotificationType} from 'react-native-fcm';
 
 export default class PushController extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      token: ''
+    };
+
+    FCM.getFCMToken().then(token => {
+      this.setState({
+        token: token
+      })
+    });
+
+  }//end constructor
 
   render() {
+    let display = this.state.token || 'Empty token';
     return (
       <Text>
-        Hello PushController!
+        {display}
       </Text>
     )
   }
